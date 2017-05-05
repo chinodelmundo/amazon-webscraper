b80548633bd80681cc6f78e3df5db3f606666961
Chino <chinodelmundo@gmail.com>
2017-05-06 01:43:22 +0800

    Included reviews

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //erase me
-
 	xray(req.body.url, '#mainResults', {
   		products: xray('.s-result-item', [{
   			name: xray('.s-access-detail-page h2'),
@@ -18,11 +16,34 @@ router.post('/', function(req, res, next) {
   					whole: xray('.sx-price-whole'),
   					decimal: xray('.sx-price-fractional')
   				},
+        rating: xray('.a-icon-star .a-icon-alt'),
   			link: xray('.s-access-detail-page@href')
   		}])
 	})(function(err, obj) {
 		res.render('index', { products: obj.products});
 	});
+});
+
+router.get('/reviews', function(req, res, next) {
+    res.redirect('/');
+});
+
+router.post('/reviews', function(req, res, next) {
+
+  var product = {
+    name: req.body.name,
+    price: req.body.price,
+    rating: req.body.rating
+  };
+
+  xray(req.body.url, '#reviewsMedley', {
+    reviews: xray('.review', [{
+      author: xray('.author'),
+      text: xray('.a-expander-partial-collapse-content')
+    }])
+  })(function(err, obj) {
+    res.render('reviews', {product: product, reviews: obj.reviews});
+  });
 });
 
 module.exports = router;
